fcd19ca0b255aa6366a2e678a47569072bd5d2fa
Niek Jannink <niek.jannink@craytive.com>
2024-02-20 08:05:42 -0500

    initial commit

---
+++ BlazorWebAppOidc/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using BlazorWebAppOidc.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorWebAppOidc.Hubs;
+
+[Authorize]
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string message)
+    {
+        var user = UserInfo.FromClaimsPrincipal(Context.User);
+        await Clients.All.SendAsync("ReceiveMessage", user.Name, message);
+    }
+}
